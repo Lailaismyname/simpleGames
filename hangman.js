845055788a21be910d01c023aeb68f67642f2663
Laila ofzo <mynameislaila@outlook.com>
2022-08-02 19:12:00 +0200

    asd

--- hangman.js
+++ hangman.js
@@ -10,7 +10,19 @@ for (let i = 0; i <= (lengteWoord - 1); i++){
 }
 console.log("aftertekst: " + tekst);
 
+//functie woord controleren
+function woordControleren(woord, controleWoord){
+    for (let i = 0; woord.length <= i;i++){
+        if (woord[0][i] == controleWoord){
+            return 0;
+        }
+        else {
+            return false;
+        }
 
+    }
+
+}
 
 //Start knop
 document.getElementById("start").addEventListener("click", ()=>{
@@ -41,10 +53,10 @@ document.getElementById("start").addEventListener("click", ()=>{
                 document.getElementById("hangman").src = hangmansrc;
                 console.log(teller);
             }
-            else if(woorden[0] === tekst.toString()){
+            else if(woordControleren(woorden, woordCheck) == 0){
                 alert("Winst");
             }
-            console.log(woordCheck + woorden[0]);
+            console.log(woordCheck + woorden[0][0]);
         })
       })
 
@@ -52,6 +64,9 @@ document.getElementById("start").addEventListener("click", ()=>{
 
 
 // uhm de alert als het woord goed is gaat niet goed. 
+    //for loop maken om alle letters individueel te vergelijken.
+    //return true als true en false als false. als alles true dan prit wintst
+    //mijn functite is kak, checken waar die fout loopt
 //moet nog iets van een game over ding toegevoegd worden.
 // shit wat mooier maken
 //mischien de hangman ook wat mooier maken
